a910adae573194e15b2b13e3f47f6cfbeb5ff29a
Cal Zafren <calzafren@gmail.com>
2023-01-08 18:18:00 -0500

    Added ground checks

--- Chill Jam/Assets/Scripts/PlayerMovement.cs
+++ Chill Jam/Assets/Scripts/PlayerMovement.cs
@@ -6,12 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
 
     private Rigidbody2D myRB;
+    private Collider2D myCol;
     private Vector2 moveDir;
     [SerializeField]
     private int speed;
     private float moveX;
     private float moveY;
     private bool canMoveVertical = false;
+    private bool isGrounded;
+    private float checkDistance = .1f;
 
     // Start is called before the first frame update
     void Start()
@@ -28,21 +31,25 @@ public class PlayerMovement : MonoBehaviour
 
     void FindComponents(){
         myRB = GetComponent<Rigidbody2D>();
+        myCol = GetComponent<Collider2D>();
     }
 
     void CheckForInput(){
         moveX = Input.GetAxisRaw("Horizontal");
         moveY = Input.GetAxisRaw("Vertical");
+        isGrounded = IsGrounded();
 
         if(canMoveVertical){
             moveDir = new Vector2(moveX, moveY);
-        }else{
+        }else if(isGrounded){
             moveDir = new Vector2(moveX, 0);
+        }else{
+            moveDir = new Vector2(moveX, -9.81f);
         }
+        //Debug.Log(isGrounded);
     }
 
     void MoveAndAnimatePlayer(){
-        //myRB.AddForce(moveDir, ForceMode2D.Impulse);
         myRB.velocity = moveDir * speed;
     }
 
@@ -53,5 +60,18 @@ public class PlayerMovement : MonoBehaviour
 
     void OnTriggerExit2D(){
         canMoveVertical = false;
+    }
+
+    private bool IsGrounded(){
+        RaycastHit2D hit = Physics2D.BoxCast(myCol.bounds.center, myCol.bounds.size, 0f, Vector2.down, myCol.bounds.extents.y + checkDistance);
+        Debug.DrawRay(myCol.bounds.center + new Vector3(myCol.bounds.extents.x, 0), Vector2.down * (myCol.bounds.extents.y + checkDistance), Color.green);
+        Debug.DrawRay(myCol.bounds.center - new Vector3(myCol.bounds.extents.x, 0), Vector2.down * (myCol.bounds.extents.y + checkDistance), Color.green);
+        Debug.Log(hit.collider.gameObject);
+
+        if(hit.collider != null){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
